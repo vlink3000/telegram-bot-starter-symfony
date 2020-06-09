33dd22f4f20867907151a1a7deaa514367ee3267
Vladimir Link <melti-santi@mail.ru>
2020-06-09 16:54:56 +0200

    minor changes in error handling logic

---
+++ src/Domain/Exception/RepositoryException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: khorov01
+ * Date: 02/06/2020
+ * Time: 19:58
+ */
+
+namespace Telegram\Bot\Skeleton\Domain\Exception;
+
+
+class RepositoryException
+{
+
+}
